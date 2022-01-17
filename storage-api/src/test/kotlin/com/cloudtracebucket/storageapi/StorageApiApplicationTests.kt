0b218d511ca009474af22dc509a9c678ddd23992
Edgar Selihov <edgar.selihov@gmail.com>
2022-01-17 20:17:53 +0200

    CLOUD-2 - Init storage-api project

---
+++ storage-api/src/test/kotlin/com/cloudtracebucket/storageapi/StorageApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cloudtracebucket.storageapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StorageApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
