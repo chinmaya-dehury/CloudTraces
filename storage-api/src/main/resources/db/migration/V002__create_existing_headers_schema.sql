dd26a2dad92a42abd79806f1b8621a5f03bf55ff
Edgar Selihov <edgar.selihov@gmail.com>
2022-01-21 17:27:07 +0200

    CLOUD-2 - add SQL migration script to create "existing_headers" table

---
+++ storage-api/src/main/resources/db/migration/V002__create_existing_headers_schema.sql
@@ -0,0 +1,26 @@
+CREATE TYPE trace_types AS ENUM (
+    'serverless_platform',
+    'cloud_storage',
+    'cloud_cluster',
+    'cloud_instances',
+    'machines',
+    'cloud_job',
+    'resources',
+    'collections'
+    );
+
+CREATE TABLE "existing_headers"
+(
+    id                  SERIAL PRIMARY KEY NOT NULL,
+    provider            VARCHAR(255)       NOT NULL,
+    headers             TEXT[],
+    dynamic_schema_name VARCHAR(255),
+    trace_type          trace_types        NOT NULL,
+    target_schema       VARCHAR(255)       NOT NULL,
+    create_time         TIMESTAMP          NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time         TIMESTAMP          NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    delete_time         TIMESTAMP                   DEFAULT NULL
+);
+
+CREATE UNIQUE INDEX headers_uindex ON "existing_headers" (headers);
+CREATE UNIQUE INDEX dynamic_schema_name_uindex ON "existing_headers" (dynamic_schema_name);
