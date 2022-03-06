7fd2f7119a3d9a4b76339f967e2cc009f198218a
Edgar Selihov <edgar.selihov@gmail.com>
2022-03-06 23:02:58 +0200

    CLOUD-6 add status endpoint, add basic auth, add logger for requests

---
+++ data-collector/src/api/controllers.js
@@ -0,0 +1,8 @@
+
+const getStatus = (req, res) => {
+    res.status(200).json({ ok: true });
+};
+
+module.exports = {
+    getStatus,
+}
--- data-collector/src/app.js
+++ data-collector/src/app.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const basicAuth = require('express-basic-auth');
+const api = require('./api/controllers');
+const morgan = require('morgan');
 const app = express();
 const port = 6002;
 
 require('dotenv').config();
-app.use(express.json());
 
+app.use(morgan('tiny'));
+app.use(basicAuth({
+    users: { admin: process.env.API_PASSWORD },
+    challenge: true,
+    unauthorizedResponse: getUnauthorizedResponse
+}));
+
+app.get('/status', api.getStatus);
 app.listen(port, () => {
    console.log(`App is running on port ${port}`);
 });
+
+function getUnauthorizedResponse(req) {
+    return JSON.stringify(req.auth ? `Credentials ${req.auth.user}:${req.auth.password} rejected` : 'Unauthorized');
+}
