5e993ca91f8c2289f2b45ea27a92b4ce10b774a2
Edgar Selihov <edgar.selihov@gmail.com>
2022-03-06 21:50:46 +0200

    CLOUD-6 Add db query generator

--- data-collector/src/db/index.js
+++ data-collector/src/db/index.js
@@ -1,2 +1,20 @@
 const { Pool } = require('pg');
 const pool = new Pool();
+
+module.exports = {
+  async query(text, params) {
+      const start = Date.now();
+
+      try {
+          const res = await pool.query(text, params);
+          const duration = Date.now() - start;
+
+          console.log('executed query', {text, duration, rows: res.rowCount});
+
+          return res;
+      } catch (error) {
+          console.error('Error in query', {text});
+          throw error;
+      }
+  }
+};
