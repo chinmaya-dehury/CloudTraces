5c94ac761618a0ad74381c24777d1dca334e9936
Edgar Selihov <edgar.selihov@gmail.com>
2022-04-21 15:21:13 +0300

    cloud-14 - new line at the end of the file

--- cloud-trace-bucket-vue-2/src/components/errorPages/NotFound.vue
+++ cloud-trace-bucket-vue-2/src/components/errorPages/NotFound.vue
@@ -11,10 +11,10 @@ export default {
 
 </script>
 <style scoped>
-p {
-  font-weight: bold;
-  font-size: 50px;
-  text-align: center;
-  color: #f10b0b;
-}
+  p {
+    font-weight: bold;
+    font-size: 50px;
+    text-align: center;
+    color: #f10b0b;
+  }
 </style>
