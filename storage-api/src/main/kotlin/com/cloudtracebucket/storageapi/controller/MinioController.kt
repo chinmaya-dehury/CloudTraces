815e057fe9f2ed460084b9843122d4007341165c
Edgar Selihov <edgar.selihov@gmail.com>
2022-04-23 20:25:40 +0300

    change cross origin url

--- storage-api/src/main/kotlin/com/cloudtracebucket/storageapi/controller/MinioController.kt
+++ storage-api/src/main/kotlin/com/cloudtracebucket/storageapi/controller/MinioController.kt
@@ -30,7 +30,7 @@ import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @RequestMapping("/files")
-@CrossOrigin(origins = ["http://localhost:8080"])
+@CrossOrigin(origins = ["http://172.17.91.248:8082"])
 class MinioController @Autowired constructor(
     private val minioService: MinioService,
     private val fileValidator: FileValidator,
