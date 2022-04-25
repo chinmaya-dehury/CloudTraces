73c8770f86d58d015356118c1b90b21c20325403
Edgar Selihov <edgar.selihov@gmail.com>
2022-04-25 13:23:34 +0300

    improvements - add trace type to files response and column to files downloading

--- storage-api/src/main/kotlin/com/cloudtracebucket/storageapi/controller/response/FileInfoResponse.kt
+++ storage-api/src/main/kotlin/com/cloudtracebucket/storageapi/controller/response/FileInfoResponse.kt
@@ -1,5 +1,6 @@
 package com.cloudtracebucket.storageapi.controller.response
 
+import com.cloudtracebucket.storageapi.pojo.enums.TraceType
 import com.fasterxml.jackson.annotation.JsonInclude
 import java.time.LocalDateTime
 
@@ -9,5 +10,6 @@ class FileInfoResponse {
     var fileFormat: String? = null
     var provider: String? = null
     var fileSize: Long? = 0L
+    var traceType: TraceType? = null
     var uploadTime: LocalDateTime? = null
 }
